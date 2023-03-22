3727c685a10cb2b388243db90a3bb2ce7675104a
hoangcuong1247 <hoangcuong1247@gmail.com>
2023-03-22 16:57:31 +0700

    update title 2

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> Hoang Trong Cuong.
-        </p>
+        <p>Hoang Trong Cuong.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
